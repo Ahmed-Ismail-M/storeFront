318ae03af9f59e4f29376b0fb88c8e9952b47762
ahmed <ahmedismailm85@gmail.com>
2022-02-26 09:50:16 +0200

    init: db connection

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,18 @@ import routes from './routes/index'
 import { logger } from './middlewares/logs'
 import errHandler from './middlewares/errors'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 const app: Express.Application = Express()
 const port: number = 8000
 const staticCashe = Express.static(path.join(process.cwd(), 'assets'), {
-  maxAge: 90000000,
+  maxAge: 90000000
 })
 const middleware = [staticCashe, logger, Express.json()]
 app.use(middleware)
 app.use('/api', routes)
 app.use(errHandler)
+app.use(bodyParser.json())
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
 })
