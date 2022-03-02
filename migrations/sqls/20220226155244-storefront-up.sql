a5988acd464bee2a42dde7415ad1444e8b28078e
ahmed <ahmedismailm85@gmail.com>
2022-03-02 07:43:37 +0200

    init: create tables

--- migrations/sqls/20220226155244-storefront-up.sql
+++ migrations/sqls/20220226155244-storefront-up.sql
@@ -1,3 +1,10 @@
 CREATE TABLE products(name VARCHAR(100), category VARCHAR(50), price decimal(2),  id SERIAL PRIMARY KEY);
 CREATE TABLE users(first_name VARCHAR(50), last_name VARCHAR(50), password VARCHAR(50),  id SERIAL PRIMARY KEY);
-CREATE TABLE orders(status VARCHAR(100), category VARCHAR(50, price decimal(2),  id SERIAL PRIMARY KEY);
+CREATE TABLE orders( product_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(20), id SERIAL PRIMARY KEY, 
+CONSTRAINT fk_product
+      FOREIGN KEY(product_id) 
+	  REFERENCES products(id) ON DELETE CASCADE,
+CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+	  REFERENCES users(id) ON DELETE CASCADE
+      );
