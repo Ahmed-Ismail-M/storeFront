2b7964d0e1d2006600b765ed43f24d38a816058e
ahmed <ahmedismailm85@gmail.com>
2022-03-08 17:41:24 +0200

    refactor: created DAO and DS

--- migrations/sqls/20220226155244-storefront-up.sql
+++ migrations/sqls/20220226155244-storefront-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE products(name VARCHAR(100), category VARCHAR(50), price REAL,  id SERIAL PRIMARY KEY);
-CREATE TABLE users(first_name VARCHAR(50), last_name VARCHAR(50), password VARCHAR(50),  id SERIAL PRIMARY KEY);
+CREATE TABLE users(first_name VARCHAR(50), last_name VARCHAR(50), password VARCHAR,  id SERIAL PRIMARY KEY);
 CREATE TABLE orders( product_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(20), qty Integer, id SERIAL PRIMARY KEY, 
 CONSTRAINT fk_product
       FOREIGN KEY(product_id) 
